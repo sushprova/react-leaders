5bb98ec2409cb762bfa3f156d17b968ffe3dd749
sushprova <sushthepro@gmail.com>
2024-06-15 12:01:28 -0500

    Added React Context

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Dashboard from './components/Dashboard.tsx'
-import LeadersList from './components/LeadersList.tsx'
-import LeaderDetail from './components/LeaderDetail.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import Dashboard from "./components/Dashboard.tsx";
+import LeadersList from "./components/LeadersList.tsx";
+import LeaderDetail from "./components/LeaderDetail.tsx";
+import { MessageProvider } from "./context/MessageContext.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>,
-    children:[
-      {index: true, element: <Navigate replace to= '/dashboard' />},
-      {path: '/dashboard', element: <Dashboard/>},
-      {path: '/leaders', element: <LeadersList/>},
-      {path: '/leaders/:id', element: <LeaderDetail/>},
-    ]
-  }
-])
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Navigate replace to="/dashboard" /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/leaders", element: <LeadersList /> },
+      { path: "/leaders/:id", element: <LeaderDetail /> },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router ={router} />
-  </React.StrictMode>,
-)
+    <MessageProvider>
+      <RouterProvider router={router} />
+    </MessageProvider>
+  </React.StrictMode>
+);
